b0d394350da2907dc104e1b1fd194ddf232f5bab
MalkaGit <regine.issan.follow@gmail.com>
2026-01-09 15:17:10 +0200

    create workspace with lib and monolith app

---
+++ Ex00002v7/serverWorkspace/packages/app/src/server.ts
@@ -0,0 +1,24 @@
+ 
+
+/** 
+
+ * APPLICATION ENTRY POINT 
+
+ * ======================= 
+
+ * Equivalent to Program.cs 
+
+ */ 
+
+ 
+ 
+
+//import Logger1 from package (thanks to index.ts in the package) 
+
+//import statement does not chnage as we move the module in the package 
+
+import { Logger1 } from "@server/lib-common"; 
+
+const l1 = new Logger1(); 
+
+l1.log("Hello from Logger1!"); 
---
+++ Ex00002v7/serverWorkspace/packages/lib-common/src/logger1.ts
@@ -0,0 +1,19 @@
+/**
+ * FILE MODULE
+ * ===========
+ * Equivalent to Logger1.cs
+ */
+
+
+//the export statement is file module entry point
+//it allows import by filename
+//line below allows: import { Logger1 } from "./logger1";
+//so, if we change the file name,
+//the import statement will break
+
+export class Logger1 {
+    log(message: string) {
+      console.log(`[Logger1] ${message}`);
+    }
+  }
+  
