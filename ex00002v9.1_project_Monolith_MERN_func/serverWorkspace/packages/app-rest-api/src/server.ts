84bf3ef2a5c31301d1dc6e624312398493da4a0c
MalkaGit <regine.issan.follow@gmail.com>
2026-01-19 14:50:16 +0200

    project2 common lib: phase 10.2 using logger (nmp run build,npm start on app)

--- ex00002v9.1_project_Monolith_MERN_func/serverWorkspace/packages/app-rest-api/src/server.ts
+++ ex00002v9.1_project_Monolith_MERN_func/serverWorkspace/packages/app-rest-api/src/server.ts
@@ -6,7 +6,10 @@
 
 //import Logger1 from package (thanks to index.ts in the package)
 //import statement does not chnage as we move the module in the package
-import { Logger1 } from "@server/lib-common";
-const l1 = new Logger1();
-l1.log("Hello");
+//import { Logger1 } from "@server/lib-common";
+//const l1 = new Logger1();
+//l1.log("Hello");
+
+import { logger } from "@server/lib-common";
+logger.info("Hello");
 
