5fbb2346364510663f16dea0ab1e6688bb93af50
MalkaGit <regine.issan.follow@gmail.com>
2026-01-19 17:01:50 +0200

    project2 common lib: phase 10.3 using testConnection (nmp run build,npm start on app)

--- ex00002v9.1_project_Monolith_MERN_func/serverWorkspace/packages/app-rest-api/src/server.ts
+++ ex00002v9.1_project_Monolith_MERN_func/serverWorkspace/packages/app-rest-api/src/server.ts
@@ -4,14 +4,20 @@
  * Equivalent to Program.cs
  */
 
+//test logger1 module
 //import Logger1 from package (thanks to index.ts in the package)
 //import statement does not chnage as we move the module in the package
 //import { Logger1 } from "@server/lib-common";
 //const l1 = new Logger1();
 //l1.log("Hello");
 
+//test logger module
 import { logger } from "@server/lib-common";
 logger.info("Hello info");
 logger.debug("Hello debug");
+
+//test mysql connection module
+import { testMySqlConnection } from "@server/lib-common";
+testMySqlConnection();
 
 
