73412b92442ad0ebf290e90b5fd9823f5950a566
MalkaGit <regine.issan.follow@gmail.com>
2026-01-22 00:10:42 +0200

    template3: phase10.9 - adding app/express.middlewares/request-logger module

--- Ex00002v9_project_templates/template003_node_rest_api_with_health_endpoint/serverWorkspace/lib-common/src/app/express.middlewares/index.ts
+++ Ex00002v9_project_templates/template003_node_rest_api_with_health_endpoint/serverWorkspace/lib-common/src/app/express.middlewares/index.ts
@@ -11,3 +11,4 @@
 export { createRequestValidator } from "./request-validator.middleware";
 export { requestContextMiddleware } from "./request-context.middleware";
 export { authMiddleware } from "./auth.middleware";
+export { requestLoggerMiddleware } from "./request-logger.middleware";
